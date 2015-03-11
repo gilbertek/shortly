64bb4c8b8c62a14545f83115265fcd6affae7093
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2015-03-11 12:27:06 -0400

    updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'shortner'
+set :repo_url, 'git@github.com:gilbertek/shortly.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
